66f6e9b68f496847b7389cbb8d5a75f8ed6098ce
MagaSanchez12 <r.sanchez@fiuna.edu.py>
2026-05-20 09:21:46 -0300

    Initial commit

---
+++ src/Dispositivo/Dispositivo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dispositivo;
+
+/**
+ *
+ * @author magal
+ */
+public class Dispositivo {
+    protected String nombre;
+    protected boolean estado;
+    protected double consumoEnergia;
+    protected String ubicacion;
+}
